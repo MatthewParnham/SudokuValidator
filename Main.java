c391bba90f8857c58c8033535b478815137ece80
Matthew <parnham@chapman.edu>
2019-10-16 15:00:59 -0700

    updated main

--- Main.java
+++ Main.java
@@ -1,9 +1,30 @@
+import java.io.*;
 
 public class Main {
 
   public static void main(String[] args) {
 
     //read in file
+    String fName = args[0];
+    String thisLine;
+    int count=0;
+    FileInputStream fis;
+    DataInputStream myInput;
+    try {
+      fis = new FileInputStream(fName);
+      myInput = new DataInputStream(fis);
+      if(myInput == null) {
+        System.out.println("File Not Found");
+        System.exit(0);
+      }
+    } catch (FileNotFoundException e) {
+      System.out.println("File Not Found");
+      System.exit(0);
+    }
+
+
+
+
 
     //validate rows
     //loop through rows
